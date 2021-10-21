e358695a0b42d735b364e60644667975f9982381
Salvador <Sagarcia2713@gmail.com>
2021-10-21 13:36:13 -0500

    1st commit

--- finalGradeCalculator/ViewController.swift
+++ finalGradeCalculator/ViewController.swift
@@ -1,20 +1,51 @@
-//
 //  ViewController.swift
 //  finalGradeCalculator
-//
 //  Created by Sal Abuali on 7/9/20.
 //  Copyright © 2020 Sal Abuali. All rights reserved.
-//
-
+//========================================================
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var currentGradeText: UILabel!
+    @IBOutlet weak var examWeightText: UILabel!
+    @IBOutlet weak var desiredGradeText: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var gradeNeeded:Double?
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func calculateButton(_ sender: UIButton)
+    {
+        
+        view.endEditing(true);
+        let desiredGrade = Double(desiredGradeText.text!)!
+        let currentGrade = Double(currentGradeText.text!)!
+        let examWeight = Double(examWeightText.text!)!
+        
+        let gradeNeeded = (100 * desiredGrade - (100 - examWeight) * currentGrade)/examWeight
+        
+        print(gradeNeeded)
+        resultLabel.text = String(format:"%.1f", gradeNeeded) + "%"
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
 
